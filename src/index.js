c4ba7e78a275de92f29ec70ba81dbaec64f88a26
DerpMarine <joel@ellis.link>
2019-07-07 19:02:32 +0100

    add livereload.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ program
   .version('0.1.0')
   .usage('[options] <file>')
   .option("-w, --watch", "Watch the files for changes.")
-  .option("-o, --out", "Watch the files for changes.")
+  .option("-o, --out [file]", "Watch the files for changes.")
   .option('-v, --verbose', 'What the program logs.\n'
     + '0 - error, 1 - warning, 2 - info, 3 - debug', function increaseVerbosity (v, total) {
       return total + 1;
@@ -31,6 +31,21 @@ let opts = {
   watch,
   out
 }
-log.info("Starting generation...")
-require("./rvl")(file, opts, log, !watch)
-log.info("Done generation!")
+if (!watch) {
+  log.info("Starting generation...")
+  require("./rvl")(file, opts, log, !watch)
+  log.info("Done generation!")
+} else {
+  // livereload
+  var livereload = require('livereload');
+  var fs = require('fs');
+  const rvl = require("./rvl")
+  rvl(file, opts, log, !watch, "<script>document.write('<script src=\"http://' + (location.host || 'localhost').split(':')[0] +':35729/livereload.js?snipver=1\"></' + 'script>')</script>")
+  fs.watch(file.toString(), (eventType) => {
+    if (eventType === "change") rvl(file, opts, log, !watch, "<script>document.write('<script src=\"http://' + (location.host || 'localhost').split(':')[0] +':35729/livereload.js?snipver=1\"></' + 'script>')</script>")
+  })
+
+  var server = livereload.createServer({
+  });
+  server.watch(opts.out);
+}
